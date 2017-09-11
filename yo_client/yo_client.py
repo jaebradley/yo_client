d10182ee97045adb8889ff041c4caa61fb0a40c1
jaebradley <jae.b.bradley@gmail.com>
2017-09-11 16:36:13 -0500

    update

--- yo_client/yo_client.py
+++ yo_client/yo_client.py
@@ -1,7 +1,7 @@
 import requests
 
 import yo_client.urls
-from yo_client.request_parameters import RequestBodyBuilder, RequestQueryParametersBuilder
+import yo_client.request_parameters
 
 
 class YoClient:
@@ -10,8 +10,10 @@ class YoClient:
 
     def send_yo(self, username, text=None, link=None, coordinate=None):
         r = requests.post(url=yo_client.urls.SEND_YO_URL,
-                          data=RequestBodyBuilder.build_send_yo_body(username=username, api_token=self.api_key,
-                                                                     text=text, link=link, coordinate=coordinate))
+                          data=yo_client.request_parameters.RequestBodyBuilder.build_send_yo_body(username=username,
+                                                                                                  api_token=self.api_key,
+                                                                                                  text=text, link=link,
+                                                                                                  coordinate=coordinate))
 
         r.raise_for_status()
 
@@ -19,8 +21,9 @@ class YoClient:
 
     def send_yo_to_all_subscribers(self, link=None):
         r = requests.post(url=yo_client.urls.SEND_YOS_TO_ALL_SUBSCRIBERS_URL,
-                          data=RequestBodyBuilder.build_send_yo_to_all_subscribers_body(api_token=self.api_key,
-                                                                                        link=link))
+                          data=yo_client.request_parameters.RequestBodyBuilder.build_send_yo_to_all_subscribers_body(
+                              api_token=self.api_key, link=link
+                          ))
 
         r.raise_for_status()
 
@@ -28,12 +31,11 @@ class YoClient:
 
     def create_account(self, username, password=None, callback_url=None, email=None, description=None, needs_location=False, welcome_link=None):
         r = requests.post(url=yo_client.urls.CREATE_ACCOUNT_URL,
-                          data=RequestBodyBuilder.build_account_creation_body(username=username, api_token=self.api_key,
-                                                                              password=password,
-                                                                              callback_url=callback_url,
-                                                                              email=email, description=description,
-                                                                              needs_location=needs_location,
-                                                                              welcome_link=welcome_link))
+                          data=yo_client.request_parameters.RequestBodyBuilder.build_account_creation_body(
+                              username=username, api_token=self.api_key, password=password, callback_url=callback_url,
+                              email=email, description=description, needs_location=needs_location,
+                              welcome_link=welcome_link)
+                          )
 
         r.raise_for_status()
 
@@ -41,8 +43,9 @@ class YoClient:
 
     def username_exists(self, username):
         r = requests.get(url=yo_client.urls.USERNAME_EXISTS_URL,
-                         data=RequestQueryParametersBuilder.build_username_exists_query_parameters(username=username,
-                                                                                                   api_token=self.api_key))
+                         data=yo_client.request_parameters.RequestQueryParametersBuilder.build_username_exists_query_parameters(
+                             username=username, api_token=self.api_key)
+                         )
 
         r.raise_for_status()
 
